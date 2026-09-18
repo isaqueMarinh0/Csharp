0245217269e2008c8e7567935aa684ba442110c7
Isaque Marinho <145267134+isaqueMarinh0@users.noreply.github.com>
2026-09-18 10:31:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiroProjeto
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo");
+        }
+    }
+}
