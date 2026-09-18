61bd2c2cf71d332cbe638b3dcb40d8f66557ee11
Isaque Marinho <145267134+isaqueMarinh0@users.noreply.github.com>
2026-09-18 10:33:52 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-namespace primeiroProjeto
+namespace primeiroProjeto //Agrupamento de classes relacionadas
 { 
-    internal class Program
+    internal class Program //Classe chamada Program (mesmo nome do arquivo)
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //É o ponto inicial de execução do programa
         {
             Console.WriteLine("Olá mundo");
         }
